28c91147a407e8e7d96b0a405be9044c56920079
amber's husband <1316920799@qq.com>
2022-11-15 14:11:27 +0800

    递归(不使用变量来求字符串长度)

---
+++ C语言递归求字符串长度.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<assert.h>
+
+
+int my_strlen(char* str)
+{
+	assert(str != NULL);
+	if (*str == '\0')
+	{
+		return 0;
+	}
+	else
+	{
+		return 1 + my_strlen(str + 1);
+	}
+}
+
+int main()
+{
+	char arr[] = "abcdef";
+	printf("%d ", my_strlen(arr));
+
+	return 0;
+}
